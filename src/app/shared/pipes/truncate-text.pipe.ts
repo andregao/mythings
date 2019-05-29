77c78843810886bf56d43479243b091320efd0ec
Andre Gao <andregao@me.com>
2019-05-29 01:49:30 -0700

    ready to update to v8

--- src/app/shared/pipes/truncate-text.pipe.ts
+++ src/app/shared/pipes/truncate-text.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TruncateTextPipe implements PipeTransform {
 
   transform(text: string, length: number): any {
-    console.log(text, length);
+    // console.log(text, length);
     if (text.length > length) {
       return text.slice(0, length) + '...';
     }
